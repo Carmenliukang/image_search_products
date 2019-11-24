3f9c892f37997894abe9e82dd92a7d915d13aabb
Carmenliukang <17621173188@163.com>
2019-11-24 22:07:38 +0800

    init project

---
+++ lib/func_txy.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import time
+import random
+import string
+import requests
+import contextlib
+
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def get_random_only_letter(k):
+    return ''.join(random.choices(string.ascii_letters, k=k))
+
+
+def request_get(url, params=None, headers={}, allow_redirects=True):
+    try:
+        with contextlib.closing(
+                requests.get(url, params=params, headers=headers, timeout=30, allow_redirects=allow_redirects)) as req:
+            data = req.json()
+        # print("[{}]\nurl:{}\nparams:{}\nresult:{}".format(now(), url, json.dumps(params, ensure_ascii=False),
+        #                                                   json.dumps(data, ensure_ascii=False)))
+        return "succ", data
+    except Exception as e:
+        print(e)
+        return "fail", {}
+
+
+def request_get_content(url, params=None, headers={}, allow_redirects=True):
+    try:
+        with contextlib.closing(
+                requests.get(url, params=params, headers=headers, timeout=30, allow_redirects=allow_redirects)) as req:
+            data = req.content
+        return "succ", data
+    except Exception as e:
+        print(e)
+        return "fail", ""
+
+
+def request_get_text(url, params=None, headers={}, allow_redirects=True):
+    try:
+        with contextlib.closing(
+                requests.get(url, params=params, headers=headers, timeout=30, allow_redirects=allow_redirects)) as req:
+            data = req.text
+        return "succ", data
+    except Exception as e:
+        print(e)
+        return "fail", ""
+
+
+def request_post(url, data=None, headers={}):
+    try:
+        with contextlib.closing(requests.post(url, data=data, headers=headers, timeout=30)) as req:
+            data = req.json()
+        return "succ", data
+    except Exception as e:
+        print(e)
+        return "fail", {}
+
+
+def filter_map(params):
+    for k in list(params.keys()):
+        if not params.get(k, ''):
+            params.pop(k)
+
+
+def now():
+    '''
+    获取当前时间
+    :return: str 2019-10-13 12:00:00
+    '''
+    return time.strftime("%Y-%m-%d %X")
---
+++ lib/taobao_lib.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from lib.func_txy import request_get
+from lib.func_txy import request_post
+
+
+class TaoBao(object):
+    def __init__(self):
+        pass
+
+
+'''
+
+https://Carmenliukang@github.com/Carmenliukang/taobao_crawler-image_search_products.git
+
+
+'''
